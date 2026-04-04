e1b2a0b7874684bf540b2937cd231e863a6491e5
reapera <faisalmerfin@gmail.com>
2026-04-04 18:45:04 +0700

    fix supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Card } from './types';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'http://localhost';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'placeholder';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
